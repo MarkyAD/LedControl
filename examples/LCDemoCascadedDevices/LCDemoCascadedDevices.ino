f86ccd6f550965010c0f6513f993f6bc530112ac
Mark <techwhiz@embarqmail.com>
2018-09-08 08:05:07 -0400

    more extraneous spaces

--- examples/LCDemoCascadedDevices/LCDemoCascadedDevices.ino
+++ examples/LCDemoCascadedDevices/LCDemoCascadedDevices.ino
@@ -4,9 +4,9 @@
 /*
  Now we need a LedControl to work with.
  ***** These pin numbers will probably not work with your hardware *****
- pin 12 is connected to the DataIn 
- pin 11 is connected to the CLK 
- pin 10 is connected to LOAD 
+ pin 12 is connected to the DataIn
+ pin 11 is connected to the CLK
+ pin 10 is connected to LOAD
  ***** Please set the number of devices you have *****
  But the maximum default of 8 MAX72XX wil also work.
  */
@@ -15,9 +15,9 @@ LedControl lc=LedControl(12,11,10,8);
 /* we always wait a bit between updates of the display */
 unsigned long delaytime=500;
 
-/* 
- This time we have more than one device. 
- But all of them have to be initialized 
+/*
+ This time we have more than one device.
+ But all of them have to be initialized
  individually.
  */
 void setup() {
@@ -34,10 +34,10 @@ void setup() {
   }
 }
 
-void loop() { 
+void loop() {
   //read the number cascaded devices
   int devices=lc.getDeviceCount();
-  
+
   //we have to init all devices in a loop
   for(int row=0;row<8;row++) {
     for(int col=0;col<8;col++) {
